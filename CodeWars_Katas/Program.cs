310c7d7c349ab16ba98c4f16e982058e0cd3f659
ecrookshanks <ed.crookshanks@gmail.com>
2021-01-21 11:06:26 -0500

    Add project files.

---
+++ CodeWars_Katas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeWars_Katas
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("This is the console app - kata tests will *mainly* be implemented as unit tests.");
+    }
+  }
+}
---
+++ Katas.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Katas.Tests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
